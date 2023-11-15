72a832c50f4b00152d7c75e1e92f5a2b144322e5
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-11-15 08:33:18 +0100

    1-pobie

--- script.js
+++ script.js
@@ -3,60 +3,58 @@ let buttonGetData = document.getElementById('getData');
 
 
 function getDataFunction() {
-  // console.log(getDataFunction());
-  // fetch('https://jsonplaceholder.typicode.com/posts')
-  fetch('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
-    .then(response => response.json())
-    // .then(data => console.log(data))
-    // .then(data => { console.log(data.title) })
-    .then(data => {
-      console.log(data)
-      // console.log("dir ")
-      // console.dir(data)
-      // console.log(data.imie)
-      // console.log(data.imie)
-      // console.log(data.nazwisko)
+    // console.log(getDataFunction());
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
+        .then(response => {
+            console.log("🚀 ~ file: script.js:10 ~ getDataFunction ~ response:", response)
+            return response.json();
+        })
+        // .then(data => console.log(data))
+        // .then(data => { console.log(data.title) })
+        .then(data => {
+            console.log(data);
+            // console.log("dir ")
+            // console.dir(data)
+            // console.log(data.imie)
+            // console.log(data.imie)
+            // console.log(data.nazwisko)
 
-      let createDiv = document.createElement('div')
-      createDiv.id = 'dane-programisty'
-      
-      // console.log(createDiv)
-      
-      // for (let person for data) {
-        let createLi = document.createElement('li')
-        createLi.innerText = `${data.imie} ${data.nazwisko}, ${data.zawod}, ${data.firma}`
-              
-        createDiv.appendChild(createLi)
-        // createLi.innerText = data.nazwisko
-        // createDiv.appendChild(createLi)
-        // createLi.innerText = data.zawod
-        // createDiv.appendChild(createLi)
-        // createLi.innerText = data.firma
-        // createDiv.appendChild(createLi)
-        
-        console.log(createDiv)
-        console.log(createLi)
-                  
-      // }
-      document.body.appendChild(createDiv)
+            let createDiv = document.createElement('div');
+            createDiv.id = 'dane-programisty';
+
+            // console.log(createDiv)
+
+            // for (let person for data) {
+            let createLi = document.createElement('li');
+            createLi.innerText = `${data.imie} ${data.nazwisko}, ${data.zawod}, ${data.firma}`
+
+            createDiv.appendChild(createLi);
+            // createLi.innerText = data.nazwisko
+            // createDiv.appendChild(createLi)
+            // createLi.innerText = data.zawod
+            // createDiv.appendChild(createLi)
+            // createLi.innerText = data.firma
+            // createDiv.appendChild(createLi)
+
+            console.log(createDiv);
+            console.log(createLi);
+
+            // }
+            document.body.appendChild(createDiv);
 
 
-      // data.nazwisko sdata.zawod data.frma
-      // `
-      // })
+            // data.nazwisko sdata.zawod data.frma
+            // `
+        })
 
-    .catch(error => console.log(error));
-  // console.log(data.title)
+        .catch(error => console.log(error));
+    // console.log(data.title)
 
 
 }
 
 buttonGetData.addEventListener('click', getDataFunction);
-
-
-
-
-
 
 
 /* <!-- Opis
